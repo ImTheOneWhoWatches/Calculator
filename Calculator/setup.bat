f48ae1d32e7d2ed922dfcfbb6e1ce395541de2c8
TheOneWhoWatches <162903308+ImTheOneWhoWatches@users.noreply.github.com>
2024-03-28 22:16:36 -0400

    Boom

---
+++ Calculator/setup.bat
@@ -0,0 +1,6 @@
+@echo off
+Title Downloading Modules...
+python -m pip install -r assets/requirements.txt
+echo ------------------------------------------------------------------------
+echo Script reached end. Open Calculator.py
+pause
